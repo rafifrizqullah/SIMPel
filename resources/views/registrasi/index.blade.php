9f0b7f47f0f44185bda70b6033d05ac0d638332d
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-09-19 14:13:25 +0700

    Update index.blade.php
    Add submission form

--- resources/views/registrasi/index.blade.php
+++ resources/views/registrasi/index.blade.php
@@ -28,6 +28,7 @@
             <label for="FormControlSelectDivisi" class="col-sm-2 col-form-label">Divisi</label>
             <div class="col-5">
               <select class="form-control" id="FormControlSelectDivisi">
+                <option>-- pilih divisi --</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -39,7 +40,8 @@
 					<div class="form-group row">
             <label for="FormControlSelectBagian" class="col-sm-2 col-form-label">Bagian</label>
             <div class="col-5">
-              <select class="form-control" id="FormControlSelectDivisi">
+              <select class="form-control" id="FormControlSelectBagian">
+                <option>-- pilih bagian --</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -52,79 +54,92 @@
           <h3>Analisis Kebutuhan Pelatihan</h3>
           
           <div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Uraian Tugas</label>
+            <label for="FormControlUraian" class="col-sm-2 col-form-label">Uraian Tugas</label>
             <div class="col-10">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="FormControlUraian" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Kompetensi yang dibutuhkan</label>
+            <label for="FormControlKompetensi" class="col-sm-2 col-form-label">Kompetensi yang dibutuhkan</label>
             <div class="col-10">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="FormControlKompetensi" rows="3"></textarea>
+            </div>
+          </div>
+          
+          <hr>
+          
+          <div class="form-group row">
+            <label for="FormControlTema" class="col-sm-2 col-form-label">Tema Pelatihan yang diusulkan</label>
+            <div class="col-10">
+              <textarea class="form-control" id="FormControlTema" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Tema Pelatihan yang diusulkan</label>
+            <label for="FormControlSasaran" class="col-sm-2 col-form-label">Sasaran yang ingin dicapai</label>
             <div class="col-10">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="FormControlSasaran" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Sasaran yang ingin dicapai</label>
+            <label for="FormControlTarget" class="col-sm-2 col-form-label">Target Peserta</label>
             <div class="col-10">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="exampleFormControlTarget" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Target Peserta</label>
+            <label for="FormControlTempat" class="col-sm-2 col-form-label">Tempat Penyelenggaraan</label>
             <div class="col-10">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-					</div>
-					<div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Tema Penyelenggaraan</label>
-            <div class="col-10">
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="FormControlTempat" rows="3"></textarea>
             </div>
           </div>
           <div class="form-group row date" data-provide="datepicker">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Tanggal Penyelenggaraan</label>
+            <label for="FormControlTanggal" class="col-sm-2 col-form-label">Tanggal Penyelenggaraan</label>
             <div class="col-5">
               <input type="date" class="form-control" id="data-date">
             </div>
           </div>
           <div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Jumlah Peserta</label>
+            <label for="FormControlJumlahPeserta" class="col-sm-2 col-form-label">Jumlah Peserta</label>
             <div class="col-5">
-              <input class="form-control" id="exampleFormControlTextarea1" rows="3">
+              <input class="form-control" id="FormControlJumlahPeserta" rows="3">
             </div>
           </div>
           <div class="form-group row">
-            <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Durasi (Jam)</label>
+            <label for="FormControlDurasi" class="col-sm-2 col-form-label">Durasi (Jam)</label>
             <div class="col-5">
-              <input class="form-control" id="exampleFormControlTextarea1" rows="3">
+              <input class="form-control" id="FormControlDurasi" rows="3">
             </div>
           </div>
           <h3>Estimasi Biaya</h3>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Uang Saku</label>
-            <input class="form-control" id="exampleFormControlTextarea1" rows="3" readonly>
+          <div class="form-group row">
+            <label for="FormControlUangSaku" class="col-sm-2 col-form-label">Uang Saku</label>
+            <div class="col-5">
+              <input class="form-control" id="FormControlUangSaku" rows="3" readonly>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Uang Fasilitator</label>
-            <input class="form-control" id="exampleFormControlTextarea1" rows="3" readonly>
+          <div class="form-group row">
+            <label for="FormControlUangFasilitator" class="col-sm-2 col-form-label">Uang Fasilitator</label>
+            <div class="col-5">
+              <input class="form-control" id="FormControlUangFasilitator" rows="3" readonly>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Narasumber Eksternal</label>
-            <input class="form-control" id="exampleFormControlTextarea1" rows="3">
+          <div class="form-group row">
+            <label for="FormControlNarasumber" class="col-sm-2 col-form-label">Narasumber Eksternal</label>
+            <div class="col-5">
+              <input class="form-control" id="FormControlNarasumber" rows="3">
+            </div>
           </div>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Akomodasi</label>
-            <input class="form-control" id="exampleFormControlTextarea1" rows="3">
+          <div class="form-group row">
+            <label for="FormControlAkomodasi" class="col-sm-2 col-form-label">Akomodasi</label>
+            <div class="col-5">
+              <input class="form-control" id="FormControlAkomodasi" rows="3">
+            </div>
           </div>
-          <div class="form-group">
-            <label for="exampleFormControlTextarea1">Jumlah</label>
-            <input class="form-control" id="exampleFormControlTextarea1" rows="3" readonly>
+          <div class="form-group row">
+            <label for="FormControlJumlah" class="col-sm-2 col-form-label">Jumlah</label>
+            <div class="col-5">
+              <input class="form-control" id="FormControlJumlah" rows="3" readonly>
+            </div>
           </div>
 				</form>
 			</div>
