ece427ce35e3d2096a80451993bbfd49a20b65d6
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-09-24 08:18:45 +0700

    Update index.blade.php

--- resources/views/registrasi/index.blade.php
+++ resources/views/registrasi/index.blade.php
@@ -1,52 +1,46 @@
 @extends('layout/main')
 
-@section('title', 'Pos Indonesia - Registrasi')
+@section('title', 'Pos Indonesia - Pengajuan Pelatihan')
 		
 @section('container')
 	<div class="container">
 		<div class="row">
-			<div class="col-12">
+			<div class="col-md-10">
         
-        <h1 class="mt-3">Registrasi Pelatihan</h1>
+        <h1 class="mt-3">Pengajuan Pelatihan</h1>
         
-        <form>
-					<div class="form-group row">
-						<label for="FormControlSelectDirektorat" class="col-sm-2 col-form-label">Direktorat</label>
+        <form action="registrasi/store" method="POST">
+          {{ csrf_field() }}
+          <div class="form-group row">
+						<label for="direktorat" class="col-sm-2 col-form-label">Direktorat</label>
             <div class="col-5">
-              <select class="form-control" id="FormControlSelectDirektorat">
+              <select class="form-control" name="direktorat" id="direktorat">
+                
                 <option>-- pilih direktorat --</option>
-                <option>Kurir dan Logistik</option>
-                <option>Jaringan dan Keuangan</option>
-                <option>Komersial</option>
-                <option>Hubungan Strategis dan Kelembagaan</option>
-                <option>Keuangan dan Umum</option>
-                <option>Sumber Daya Manusia</option>
+                @foreach ($direktorat as $key => $value)
+                  <option value="{{ $key }}">{{ $value }}</option>
+                @endforeach
+
               </select>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="FormControlSelectDivisi" class="col-sm-2 col-form-label">Divisi</label>
+						<label for="divisi" class="col-sm-2 col-form-label">Divisi</label>
             <div class="col-5">
-              <select class="form-control" id="FormControlSelectDivisi">
+              <select class="form-control" name="divisi" id="divisi">
+                
                 <option>-- pilih divisi --</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+
               </select>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="FormControlSelectBagian" class="col-sm-2 col-form-label">Bagian</label>
+						<label for="bagian" class="col-sm-2 col-form-label">Bagian</label>
             <div class="col-5">
-              <select class="form-control" id="FormControlSelectBagian">
+              <select class="form-control" name="bagian" id="bagian">
+                
                 <option>-- pilih bagian --</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+
               </select>
             </div>
           </div>
@@ -54,15 +48,15 @@
           <h3>Analisis Kebutuhan Pelatihan</h3>
           
           <div class="form-group row">
-            <label for="FormControlUraian" class="col-sm-2 col-form-label">Uraian Tugas</label>
-            <div class="col-10">
-              <textarea class="form-control" id="FormControlUraian" rows="3"></textarea>
+            <label for="uraian" class="col-sm-2 col-form-label">Uraian Tugas</label>
+            <div class="col-9">
+              <textarea class="form-control" name="uraian" id="uraian" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="FormControlKompetensi" class="col-sm-2 col-form-label">Kompetensi yang dibutuhkan</label>
-            <div class="col-10">
-              <textarea class="form-control" id="FormControlKompetensi" rows="3"></textarea>
+            <label for="kompetensi" class="col-sm-2 col-form-label">Kompetensi yang dibutuhkan</label>
+            <div class="col-9">
+              <textarea class="form-control" name="kompetensi" id="kompetensi" rows="3"></textarea>
             </div>
           </div>
           
@@ -70,25 +64,25 @@
           
           <div class="form-group row">
             <label for="FormControlTema" class="col-sm-2 col-form-label">Tema Pelatihan yang diusulkan</label>
-            <div class="col-10">
+            <div class="col-9">
               <textarea class="form-control" id="FormControlTema" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
             <label for="FormControlSasaran" class="col-sm-2 col-form-label">Sasaran yang ingin dicapai</label>
-            <div class="col-10">
+            <div class="col-9">
               <textarea class="form-control" id="FormControlSasaran" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
             <label for="FormControlTarget" class="col-sm-2 col-form-label">Target Peserta</label>
-            <div class="col-10">
+            <div class="col-9">
               <textarea class="form-control" id="exampleFormControlTarget" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
             <label for="FormControlTempat" class="col-sm-2 col-form-label">Tempat Penyelenggaraan</label>
-            <div class="col-10">
+            <div class="col-9">
               <textarea class="form-control" id="FormControlTempat" rows="3"></textarea>
             </div>
           </div>
@@ -113,36 +107,91 @@
           <h3>Estimasi Biaya</h3>
           <div class="form-group row">
             <label for="FormControlUangSaku" class="col-sm-2 col-form-label">Uang Saku</label>
-            <div class="col-5">
+            <div class="col-4">
               <input class="form-control" id="FormControlUangSaku" rows="3" readonly>
             </div>
           </div>
           <div class="form-group row">
             <label for="FormControlUangFasilitator" class="col-sm-2 col-form-label">Uang Fasilitator</label>
-            <div class="col-5">
+            <div class="col-4">
               <input class="form-control" id="FormControlUangFasilitator" rows="3" readonly>
             </div>
           </div>
           <div class="form-group row">
             <label for="FormControlNarasumber" class="col-sm-2 col-form-label">Narasumber Eksternal</label>
-            <div class="col-5">
+            <div class="col-4">
               <input class="form-control" id="FormControlNarasumber" rows="3">
             </div>
           </div>
           <div class="form-group row">
             <label for="FormControlAkomodasi" class="col-sm-2 col-form-label">Akomodasi</label>
-            <div class="col-5">
+            <div class="col-4">
               <input class="form-control" id="FormControlAkomodasi" rows="3">
             </div>
           </div>
           <div class="form-group row">
             <label for="FormControlJumlah" class="col-sm-2 col-form-label">Jumlah</label>
-            <div class="col-5">
+            <div class="col-4">
               <input class="form-control" id="FormControlJumlah" rows="3" readonly>
             </div>
           </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
 				</form>
 			</div>
 		</div>
-	</div>
+	</div>  
+@endsection
+
+@section('scripts')
+  <script type="text/javascript">
+    $(document).ready(function()
+    {
+      $('select[name="direktorat"]').on('change', function(){
+        var idDirektorat = $(this).val();
+        if (idDirektorat) {
+          $.ajax({
+            url : '/registrasi/getdirektorat/' +idDirektorat,
+            type : "GET",
+            dataType : "json",
+            success:function(data)
+            {
+              console.log(data);
+              $('select[name="divisi"]').html('<option value="">-- pilih divisi --</option>');
+              $('select[name="bagian"]').html('<option value="">-- pilih bagian --</option>');
+              $.each(data, function(key, value){
+                $('select[name="divisi"]').append('<option value="'+ key +'">' + value + '</option>');
+              });
+            }
+          });
+        }
+        else
+        {
+          $('select[name="divisi"]').html('<option value="">-- pilih divisi --</option>');
+          $('select[name="bagian"]').html('<option value="">-- pilih bagian --</option>');
+        }
+      });
+      $('select[name="divisi"]').on('change', function(){
+        var idDivisi = $(this).val();
+        if (idDivisi) {
+          $.ajax({
+            url : '/registrasi/getdivisi/' +idDivisi,
+            type : "GET",
+            dataType : "json",
+            success:function(data)
+            {
+              console.log(data);
+              $('select[name="bagian"]').html('<option value="">-- pilih bagian --</option>');
+              $.each(data, function(key, value){
+                $('select[name="bagian"]').append('<option value="'+ key +'">' + value + '</option>');
+              });
+            }
+          });
+        }
+        else
+        {
+          $('select[name="bagian"]').html('<option value="">-- pilih bagian --</option>');
+        }
+      });
+    });
+  </script>
 @endsection
