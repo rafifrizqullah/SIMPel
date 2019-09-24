0d76a4a68c06e41bbd41c9019de026bad3df5ba0
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-09-24 08:18:40 +0700

    Update index.blade.php

--- resources/views/training/index.blade.php
+++ resources/views/training/index.blade.php
@@ -6,35 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-10">
-                <h1 class="mt-3">Daftar Training</h1>
-                
-                <table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">NIP</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Training</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($training as $training)
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>{{  $training -> nama }}</td>
-                            <td>{{  $training -> nip }}</td>
-                            <td>{{  $training -> email }}</td>
-                            <td>{{  $training -> training }}</td>
-                            <td>
-                                <a href="" class="badge badge-success">edit</a>
-                                <a href="" class="badge badge-danger">delete</a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <h1 class="mt-3">Informasi Pelatihan</h1>
 			</div>
 		</div>
 	</div>
