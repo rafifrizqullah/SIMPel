b1884541a94a8e1c887126e9e7682fb89a1abbb7
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-09-26 09:00:54 +0700

    Update index.blade.php

--- resources/views/registrasi/index.blade.php
+++ resources/views/registrasi/index.blade.php
@@ -8,6 +8,7 @@
 			<div class="col-md-10">
         
         <h1 class="mt-3">Pengajuan Pelatihan</h1>
+        <br>
 
         <form action="registrasi/store" method="POST">
           {{ csrf_field() }}
@@ -20,6 +21,9 @@
                 @foreach ($direktorat as $key => $value)
                   <option value="{{ $key }}">{{ $value }}</option>
                 @endforeach
+                {{-- @foreach ($direktorat as $direktorat)
+                <option value="{{ $direktorat->id }}">{{ ucfirst($direktorat->nama) }}</option>
+                @endforeach --}}
 
               </select>
             </div>
@@ -44,13 +48,14 @@
               </select>
             </div>
           </div>
+          <br>
 
           <h3>Analisis Kebutuhan Pelatihan</h3>
           
           <div class="form-group row">
-            <label for="uraian" class="col-sm-2 col-form-label">Uraian Tugas</label>
+            <label for="uraianTugas" class="col-sm-2 col-form-label">Uraian Tugas</label>
             <div class="col-9">
-              <textarea class="form-control" name="uraian" id="uraian" rows="3"></textarea>
+              <textarea class="form-control" name="uraianTugas" id="uraianTugas" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
@@ -60,85 +65,88 @@
             </div>
           </div>
           
-          <hr>
+          <hr><br>
           
           <div class="form-group row">
-            <label for="FormControlTema" class="col-sm-2 col-form-label">Tema Pelatihan yang diusulkan</label>
+            <label for="temaPelatihan" class="col-sm-2 col-form-label">Tema Pelatihan yang diusulkan</label>
             <div class="col-9">
-              <textarea class="form-control" id="FormControlTema" rows="3"></textarea>
+              <textarea class="form-control" id="temaPelatihan" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="FormControlSasaran" class="col-sm-2 col-form-label">Sasaran yang ingin dicapai</label>
+            <label for="sasaran" class="col-sm-2 col-form-label">Sasaran yang ingin dicapai</label>
             <div class="col-9">
-              <textarea class="form-control" id="FormControlSasaran" rows="3"></textarea>
+              <textarea class="form-control" id="sasaran" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="FormControlTarget" class="col-sm-2 col-form-label">Target Peserta</label>
+            <label for="targetPeserta" class="col-sm-2 col-form-label">Target Peserta</label>
             <div class="col-9">
-              <textarea class="form-control" id="exampleFormControlTarget" rows="3"></textarea>
+              <textarea class="form-control" id="targetPeserta" rows="3"></textarea>
             </div>
 					</div>
 					<div class="form-group row">
-            <label for="FormControlTempat" class="col-sm-2 col-form-label">Tempat Penyelenggaraan</label>
+            <label for="tempatPeyelenggaraan" class="col-sm-2 col-form-label">Tempat Penyelenggaraan</label>
             <div class="col-9">
-              <textarea class="form-control" id="FormControlTempat" rows="3"></textarea>
+              <textarea class="form-control" id="tempatPenyelenggaraan" rows="3"></textarea>
             </div>
           </div>
           <div class="form-group row date" data-provide="datepicker">
-            <label for="FormControlTanggal" class="col-sm-2 col-form-label">Tanggal Penyelenggaraan</label>
+            <label for="tanggalPenyelenggaraan" class="col-sm-2 col-form-label">Tanggal Penyelenggaraan</label>
             <div class="col-5">
-              <input type="date" class="form-control" id="data-date">
+              <input type="date" class="form-control" id="tanggalPenyelenggaraan">
             </div>
           </div>
           <div class="form-group row">
-            <label for="FormControlJumlahPeserta" class="col-sm-2 col-form-label">Jumlah Peserta</label>
+            <label for="jumlahPeserta" class="col-sm-2 col-form-label">Jumlah Peserta</label>
             <div class="col-5">
-              <input class="form-control" id="FormControlJumlahPeserta" rows="3">
+              <input class="form-control" id="jumlahPeserta" rows="3">
             </div>
           </div>
           <div class="form-group row">
-            <label for="FormControlDurasi" class="col-sm-2 col-form-label">Durasi (Jam)</label>
+            <label for="durasi" class="col-sm-2 col-form-label">Durasi (Jam)</label>
             <div class="col-5">
-              <input class="form-control" id="FormControlDurasi" rows="3">
+              <input class="form-control" id="durasi" rows="3">
             </div>
           </div>
+          <br>
+
           <h3>Estimasi Biaya</h3>
+          
           <div class="form-group row">
-            <label for="FormControlUangSaku" class="col-sm-2 col-form-label">Uang Saku</label>
+            <label for="uangSaku" class="col-sm-2 col-form-label">Uang Saku</label>
             <div class="col-4">
-              <input class="form-control" id="FormControlUangSaku" rows="3" readonly>
+              <input class="form-control" id="uangSaku" rows="3" readonly>
             </div>
           </div>
           <div class="form-group row">
-            <label for="FormControlUangFasilitator" class="col-sm-2 col-form-label">Uang Fasilitator</label>
+            <label for="uangFasilitator" class="col-sm-2 col-form-label">Uang Fasilitator</label>
             <div class="col-4">
-              <input class="form-control" id="FormControlUangFasilitator" rows="3" readonly>
+              <input class="form-control" id="uangFasilitator" rows="3" readonly>
             </div>
           </div>
           <div class="form-group row">
-            <label for="FormControlNarasumber" class="col-sm-2 col-form-label">Narasumber Eksternal</label>
+            <label for="narasumber" class="col-sm-2 col-form-label">Narasumber Eksternal</label>
             <div class="col-4">
-              <input class="form-control" id="FormControlNarasumber" rows="3">
+              <input class="form-control" id="narasumber" rows="3">
             </div>
           </div>
           <div class="form-group row">
-            <label for="FormControlAkomodasi" class="col-sm-2 col-form-label">Akomodasi</label>
+            <label for="akomodasi" class="col-sm-2 col-form-label">Akomodasi</label>
             <div class="col-4">
-              <input class="form-control" id="FormControlAkomodasi" rows="3">
+              <input class="form-control" id="akomodasi" rows="3">
             </div>
           </div>
           <div class="form-group row">
-            <label for="FormControlJumlah" class="col-sm-2 col-form-label">Jumlah</label>
+            <label for="jumlah" class="col-sm-2 col-form-label">Jumlah</label>
             <div class="col-4">
-              <input class="form-control" id="FormControlJumlah" rows="3" readonly>
+              <input class="form-control" id="jumlah" rows="3" readonly>
             </div>
           </div>
           <div class="form-group row">
-            <label for="proposal" class="col-sm-2 col-form-label">File Proposal</label>
+            <label for="fileProposal" class="col-sm-2 col-form-label">File Proposal</label>
             <div class="col-sm-4">
-              <input type="file" class="form-control" id="proposal">
+              <input type="file" class="form-control" id="fileProposal">
             </div>
           </div>
           <button type="submit" class="btn btn-success">Kirim</button>
@@ -147,6 +155,64 @@
 		</div>
 	</div>  
 @endsection
+
+{{-- @section('scripts')
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('#direktorat').on('change', function(){
+        var idDirektorat = $(this).val();
+        if (idDirektorat) {
+          $.ajax({
+            url : '/registrasi/getdirektorat/' +idDirektorat,
+            type : "GET",
+            data : {"_token":"{{ csrf_token() }}"},
+            dataType : "json",
+            success:function(data) {
+              console.log(data);
+              if (data) {
+                $('#divisi').empty();
+                $('#divisi').focus;
+                $('#divisi').append('<option value="">-- pilih divisi --</option>');
+                $.each(data, function(key, value) {
+                  $('select[name="divisi"]').append('<option value="'+ key +'">' + value.nama_divisi + '</option>');
+                });
+              } else {
+                $('#divisi').empty();
+              }
+            }
+          });
+        } else {
+          $('#divisi').empty();
+        }
+      });
+      $('#divisi').on('change', function(){
+        var idDivisi = $(this).val();
+        if (idDivisi) {
+          $.ajax({
+            url : '/registrasi/getdivisi/' +idDivisi,
+            type : "GET",
+            dataType : "json",
+            success:function(data) {
+              console.log(data);
+              if (data) {
+                $('#bagian').empty();
+                $('#bagian').focus;
+                $('#bagian').append('<option value="">-- pilih bagian --</option>');
+                $.each(data, function(key, value) {
+                  $('select[name="bagian"]').append('<option value="'+ key +'">' + value.nama_bagian + '</option>');
+                });
+              } else {
+                $('#bagian').empty();
+              }
+            }
+          });
+        } else {
+          $('#bagian').empty();
+        }
+      });
+    });
+  </script>
+@endsection --}}
 
 @section('scripts')
   <script type="text/javascript">
