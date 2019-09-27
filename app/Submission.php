209a1c19e07fce501bb2f9fc9d006350bad7069d
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-09-27 11:23:40 +0700

    Update Submission.php

--- app/Submission.php
+++ app/Submission.php
@@ -6,8 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
-    //
-    protected $table = "submission";
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'submission';
 
-    protected $fillable = ['direktorat','divisi', 'bagian', 'uraian_tugas', 'kompetensi', 'tema_pelatihan'];
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['direktorat','divisi', 'bagian', 'uraian_tugas', 'kompetensi', 'sasaran', 'target_peserta', 'tempat_penyelenggaraan', 'tanggal_penyelenggaraan', 'jumlah_peserta', 'durasi', 'tema_pelatihan', 'file_proposal'];
 }
