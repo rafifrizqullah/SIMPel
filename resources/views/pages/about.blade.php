337ba197642087e569a9530b2cb95e1c4a07f1a7
anggihfal <GleanGalatea@users.noreply.github.com>
2019-10-15 17:03:43 +0700

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,6 +6,21 @@
     <div class="col-md-12">
       <h1 class="mt-3">Tentang Pos Indonesia</h1>
     </div>
+    <div>
+		<p>
+			Mempunyai jaringan yang sangat luas hingga 4.800 kantor pos online. 
+			Jumlah titik layanan (Point of Sales) mencapai 58.700 titik dalam bentuk kantor pos, 
+			Agenpos, Mobile Postal Service, dan lain lain. Pos Indonesia memiliki jaringan yang dedicated, sistem distribusi 
+			yang handal, Track and Trace, layanan yang prima, kecepatan, ketepatan, serta harga yang kompetitif. 
+			Kantor pos merupakan tempat strategis untuk transaksi penjualan, dan atau distribusi barang dan jasa.
+		</p>
+		<p>
+			Inovasi terus dilakukan oleh Pos Indonesia antara lain pembangunan Post Shop yang merupakan pengembangan 
+			bisnis ritel yang diimplementasikan untuk merubah image Kantor pos konvensional menjadi Kantor pos modern 
+			dengan pola layanan one stop shopping, yaitu Postal Services (jasa ritel) berupa layanan pengiriman surat, 
+			paket, jasa keuangan, penjualan Postal items (meterai, prangko, produk filateli dan lain lain), layanan Online Shopping.
+		</p>
+    </div>
   </div>
 </div>
 @endsection
