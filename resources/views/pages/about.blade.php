de34aed55903b53db96ecd8ab5405f229a5ba0f7
anggihfal <GleanGalatea@users.noreply.github.com>
2019-10-16 14:35:35 +0700

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,25 +2,49 @@
 
 @section('content')
 <div class="container home">
-  <div class="row">
-    <div class="col-md-12">
-      <h1 class="mt-3">Tentang Pos Indonesia</h1>
-    </div>
-    <div>
-		<p>
-			Mempunyai jaringan yang sangat luas hingga 4.800 kantor pos online. 
-			Jumlah titik layanan (Point of Sales) mencapai 58.700 titik dalam bentuk kantor pos, 
-			Agenpos, Mobile Postal Service, dan lain lain. Pos Indonesia memiliki jaringan yang dedicated, sistem distribusi 
-			yang handal, Track and Trace, layanan yang prima, kecepatan, ketepatan, serta harga yang kompetitif. 
-			Kantor pos merupakan tempat strategis untuk transaksi penjualan, dan atau distribusi barang dan jasa.
-		</p>
-		<p>
-			Inovasi terus dilakukan oleh Pos Indonesia antara lain pembangunan Post Shop yang merupakan pengembangan 
-			bisnis ritel yang diimplementasikan untuk merubah image Kantor pos konvensional menjadi Kantor pos modern 
-			dengan pola layanan one stop shopping, yaitu Postal Services (jasa ritel) berupa layanan pengiriman surat, 
-			paket, jasa keuangan, penjualan Postal items (meterai, prangko, produk filateli dan lain lain), layanan Online Shopping.
-		</p>
-    </div>
-  </div>
+	<div class="row  justify-content-center">
+		<div class="col-12 text-center">
+			<h1 class="mt-5">Tentang Pos Indonesia</h1>
+		</div>
+		<div class="col-8 text-center">
+			<p>
+				Mempunyai jaringan yang sangat luas hingga 4.800 kantor pos online. 
+				Jumlah titik layanan (Point of Sales) mencapai 58.700 titik dalam bentuk kantor pos, 
+				Agenpos, Mobile Postal Service, dan lain lain. Pos Indonesia memiliki jaringan yang dedicated, sistem distribusi 
+				yang handal, Track and Trace, layanan yang prima, kecepatan, ketepatan, serta harga yang kompetitif. 
+				Kantor pos merupakan tempat strategis untuk transaksi penjualan, dan atau distribusi barang dan jasa.
+			</p>
+			<p>
+				Inovasi terus dilakukan oleh Pos Indonesia antara lain pembangunan Post Shop yang merupakan pengembangan 
+				bisnis ritel yang diimplementasikan untuk merubah image Kantor pos konvensional menjadi Kantor pos modern 
+				dengan pola layanan one stop shopping, yaitu Postal Services (jasa ritel) berupa layanan pengiriman surat, 
+				paket, jasa keuangan, penjualan Postal items (meterai, prangko, produk filateli dan lain lain), layanan Online Shopping.
+			</p>
+		</div>
+	</div>
+	<div class="row justify-content-center">
+		<div class="col-12 text-center">
+			<h1 class="mt-5">Visi Misi Pos Indonesia</h1>
+		</div>
+		<div class="col-12 text-center">
+			<h3 class="mt-3">Visi</h3>
+		</div>
+		<div>
+			<p>
+				Menjadi pilihan utama layanan logistik dan jasa keuangan.
+			</p>
+		</div>
+		<div class="col-12 text-center">
+			<h3 class="mt-3">Misi</h3>
+		</div>
+		<div class="col-6">
+			<ul>
+				<li>Memberikan solusi layanan logistik e-commerce yang kompetitif.</li>
+				<li>Menjalankan fungsi designated operator secara profesional dan kompetitif.</li>
+				<li>Memberikan solusi jasa layanan keuangan terintegrasi yang kompetitif dalam rangka mendukung financial inclusion berbasis digital.</li>
+				<li>Memberikan solusi layanan dokumentasi dan otentikasi digital yang kompetitif.</li>
+			</ul>
+		</div>
+	</div>
 </div>
 @endsection
