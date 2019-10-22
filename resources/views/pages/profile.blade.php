f9977226b50998d144d4563fe3ba76f1771a95b2
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-10-22 16:48:27 +0700

    Update profile.blade.php

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-sm-10">
-			<h1 class="mt-5">Halo, User!</h1>
+			<h1 class="mt-5">Halo, {{ Auth::user()->name }}!</h1>
 		</div>
 		<div class="mt-3 col-sm-10">
 			<p>
@@ -17,35 +17,34 @@
           <div class="col-md-12">
             <h3>Data diri</h3>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-4">
             <img class="" src="{{ asset('/images/naruto.jfif') }}" alt="">
-          </div>
-          <div class="col-md-2 ml-4">
-            <ul class="list-unstyled">
-              <li>NIPPOS</li>
-              <li>Nama pengguna</li>
-              <li>Nama lengkap</li>
-              <li>TTL</li>
-              <li>Alamat tinggal</li>
-              <li>Kantor</li>
-              <li>Posisi</li>
-            </ul>
-          </div>
-          <div class="col-md-5">
-            <ul class="list-unstyled">
-                <li>: 666999</li>
-                <li>: Naruto</li>
-                <li>: Uzumaki Naruto<li>
-                <li>: Desa Konoha, 10 Oktober</li>
-                <li>: Desa Konoha<li>
-                <li>: Desa Konoha</li>
-                <li>: Hokage</li>
-            </ul>
           </div>
         </div>
         <div class="row mt-5">
           <div class="col-md-12">
             <h3>Statistik</h3>
+          </div>
+          <div class="col-md-3">
+            <div class="card mt-3">
+              <div class="card-body text-center">
+                <p>Permohonan pelatihan</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card mt-3">
+              <div class="card-body text-center">
+                <p>Pelatihan yang telah diikuti</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card mt-3">
+              <div class="card-body text-center">
+                <p>Pelatihan yan gakan diikuti</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
