7fc69da18b1311efd17362336893a73c91ebbb3e
rafifrizqullah <rafifrizqullah@users.noreply.github.com>
2019-10-22 15:48:09 +0700

    Update web.php
    Add profile route

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::get('/galeri', 'PagesController@galeri');
 Route::get('/about', 'PagesController@about');
 Route::get('/pelatihan', 'PagesController@pelatihan');
 Route::get('/jadwal', 'PagesController@jadwal');
+Route::get('/profile', 'PagesController@profile');
 
 Route::get('/submission', 'TrainingController@create');
 Route::get('/training/getdirektorat/{id}', 'TrainingController@getDivisi');
