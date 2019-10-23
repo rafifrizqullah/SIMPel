314a3f2ea9694b94bab9a6c92a7a82cf106b7727
fairuzzfm <fairuzzfm@users.noreply.github.com>
2019-10-23 16:31:33 +0700

    Update Galery

--- resources/views/pages/galeri.blade.php
+++ resources/views/pages/galeri.blade.php
@@ -6,9 +6,65 @@
 <div class="container home">
 	<div class="row">
 		<div class="col-md-12 text-center">
-			<h1 class="mt-5">Galeri Pelatian Pos Indonesia</h1>
-			<div class="mt-5">
-					<p style="font-size: 50px">Maaf, Halaman ini Dalam Perbaikan :)</p>
+			<h1 class="mt-5">Galeri Pelatian Pos Indonesia</h1><br><br>
+		</div>
+		<div class="row">
+			<div class="col-md-4 text-center text-center">
+				<a href="#">
+					<img src="images/Training_beranda.jpg" alt="" style="max-width:100%; border-radius:10px;  border:3px solid orange;">
+				</a>
+				<h6 style="color:orange">10-10-2019 - Bandung</h6>
+					<a href="#">
+						<p style="color:orange; font-weight:bold; font-size:16px;">Pelatihan PT.POS Indonesia</p>
+					</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<a href="#">
+					<img src="images/Training_beranda.jpg" alt="" style="max-width:100%; border-radius:10px;  border:3px solid orange;">
+				</a>
+				<h6 style="color:orange">10-10-2019 - Bandung</h6>
+					<a href="#">
+						<p style="color:orange; font-weight:bold; font-size:16px">Pelatihan PT.POS Indonesia</p>
+					</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<a href="#">
+					<img src="images/Training_beranda.jpg" alt="" style="max-width:100%; border-radius:10px;  border:3px solid orange;">
+				</a>
+				<h6 style="color:orange">10-10-2019 - Bandung</h6>
+					<a href="#">
+						<p style="color:orange; font-weight:bold; font-size:16px">Pelatihan PT.POS Indonesia</p>
+					</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<a href="#">
+					<img src="images/Training_beranda.jpg" alt="" style="max-width:100%; border-radius:10px;  border:3px solid orange;">">
+				</a>
+				<h6 style="color:orange">10-10-2019 - Bandung</h6>
+					<a href="#">
+						<p style="color:orange; font-weight:bold; font-size:16px">Pelatihan PT.POS Indonesia</p>
+					</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<a href="#">
+					<img src="images/Training_beranda.jpg" alt="" style="max-width:100%; border-radius:10px;  border:3px solid orange;">">
+				</a>
+				<h6 style="color:orange">10-10-2019 - Bandung</h6>
+					<a href="#">
+						<p style="color:orange; font-weight:bold; font-size:16px">Pelatihan PT.POS Indonesia</p>
+					</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<a href="#">
+					<img src="images/Training_beranda.jpg" alt="" style="max-width:100%; border-radius:10px;  border:3px solid orange;">
+				</a>
+				<h6 style="color:orange">10-10-2019 - Bandung</h6>
+					<a href="#">
+						<p style="color:orange; font-weight:bold; font-size:16px">Pelatihan PT.POS Indonesia</p>
+					</a>
+			</div>
+			
+					
 			</div>
 		</div>
 	</div>
