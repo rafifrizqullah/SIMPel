5bced0f31011c8a3ec6f5d80f058742cc8739b4a
anggihfal <GleanGalatea@users.noreply.github.com>
2019-11-01 16:29:42 +0700

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -27,18 +27,18 @@ class PagesController extends Controller
     public function regional()
     {
     
-        Mapper::map(-2.161694, 120.074611, ['zoom' => 4.5, 'center' => true, 'marker' => true, 'type' => 'HYBRID'])
-        ->marker(3.595733, 98.684246)
-        ->marker(-0.907093, 100.352193)
-        ->marker(-2.981376, 104.745649)
-        ->marker(-6.167431, 106.835161)
-        ->marker(-6.894023, 107.634532)
-        ->marker(-7.014934, 110.423983)
-        ->marker(-7.235923, 112.734074)
-        ->marker(-8.666576, 115.231517)
-        ->marker(-3.440981, 114.831636)
-        ->marker(-5.164130, 119.434747)
-        ->marker(-2.522577, 140.713324);
+        Mapper::map(-2.161694, 120.074611, ['zoom' => 4.5, 'marker' => false, 'center' => true, 'type' => 'HYBRID'])
+        ->informationWindow(3.595733, 98.684246, 'Regional I Medan') //Reg I Medan
+        ->informationWindow(-0.907093, 100.352193, 'Regional II Padang') //Reg II Padang
+        ->informationWindow(-2.981376, 104.745649, 'Regional III Palembang') //Reg III Palembang
+        ->informationWindow(-6.167431, 106.835161, 'Regional IV Jakarta') //Reg IV Jakarta
+        ->informationWindow(-6.894023, 107.634532, 'Regional V Bandung') //Reg V Bandung
+        ->informationWindow(-7.014934, 110.423983, 'Regional VI Semarang') //Reg VI Semarang
+        ->informationWindow(-7.235923, 112.734074, 'Regional VII Surabaya') //Reg VII Surabaya
+        ->informationWindow(-8.666576, 115.231517, 'Regional VIII Denpasar') //Reg VIII Denpasar
+        ->informationWindow(-3.440981, 114.831636, 'Regional IX Banjarbaru') //Reg IX Banjarbaru
+        ->informationWindow(-5.164130, 119.434747, 'Regional X Makassar') //Reg X Makassar
+        ->informationWindow(-2.522577, 140.713324, 'Regional XI Jayapura'); //Reg XI Jayapura
       
         return view('pages.regional');
     }
@@ -50,18 +50,19 @@ class PagesController extends Controller
      */
     public function upt()
     {
-        Mapper::map(-2.161694, 120.074611, ['zoom' => 4.5, 'center' => true, 'marker' => true, 'type' => 'HYBRID'])
-        ->marker(3.595733, 98.684246)
-        ->marker(-0.907093, 100.352193)
-        ->marker(-2.981376, 104.745649)
-        ->marker(-6.167431, 106.835161)
-        ->marker(-6.894023, 107.634532)
-        ->marker(-7.014934, 110.423983)
-        ->marker(-7.235923, 112.734074)
-        ->marker(-8.666576, 115.231517)
-        ->marker(-3.440981, 114.831636)
-        ->marker(-5.164130, 119.434747)
-        ->marker(-2.522577, 140.713324);
+
+        Mapper::map(-2.161694, 120.074611, ['zoom' => 4.5, 'marker' => false, 'center' => true, 'type' => 'HYBRID'])
+        ->informationWindow(3.595733, 98.684246, 'Regional I Medan') //Reg I Medan
+        ->informationWindow(-0.907093, 100.352193, 'Regional II Padang') //Reg II Padang
+        ->informationWindow(-2.981376, 104.745649, 'Regional III Palembang') //Reg III Palembang
+        ->informationWindow(-6.167431, 106.835161, 'Regional IV Jakarta') //Reg IV Jakarta
+        ->informationWindow(-6.894023, 107.634532, 'Regional V Bandung') //Reg V Bandung
+        ->informationWindow(-7.014934, 110.423983, 'Regional VI Semarang') //Reg VI Semarang
+        ->informationWindow(-7.235923, 112.734074, 'Regional VII Surabaya') //Reg VII Surabaya
+        ->informationWindow(-8.666576, 115.231517, 'Regional VIII Denpasar') //Reg VIII Denpasar
+        ->informationWindow(-3.440981, 114.831636, 'Regional IX Banjarbaru') //Reg IX Banjarbaru
+        ->informationWindow(-5.164130, 119.434747, 'Regional X Makassar') //Reg X Makassar
+        ->informationWindow(-2.522577, 140.713324, 'Regional XI Jayapura'); //Reg XI Jayapura
 
         return view('pages.upt');
     }
@@ -73,18 +74,19 @@ class PagesController extends Controller
      */
     public function pusat()
     {
-        Mapper::map(-2.161694, 120.074611, ['zoom' => 4.5, 'center' => true, 'marker' => true, 'type' => 'HYBRID'])
-        ->marker(3.595733, 98.684246)
-        ->marker(-0.907093, 100.352193)
-        ->marker(-2.981376, 104.745649)
-        ->marker(-6.167431, 106.835161)
-        ->marker(-6.894023, 107.634532)
-        ->marker(-7.014934, 110.423983)
-        ->marker(-7.235923, 112.734074)
-        ->marker(-8.666576, 115.231517)
-        ->marker(-3.440981, 114.831636)
-        ->marker(-5.164130, 119.434747)
-        ->marker(-2.522577, 140.713324);
+       
+        Mapper::map(-2.161694, 120.074611, ['zoom' => 4.5, 'marker' => false, 'center' => true, 'type' => 'HYBRID'])
+        ->informationWindow(3.595733, 98.684246, 'Regional I Medan') //Reg I Medan
+        ->informationWindow(-0.907093, 100.352193, 'Regional II Padang') //Reg II Padang
+        ->informationWindow(-2.981376, 104.745649, 'Regional III Palembang') //Reg III Palembang
+        ->informationWindow(-6.167431, 106.835161, 'Regional IV Jakarta') //Reg IV Jakarta
+        ->informationWindow(-6.894023, 107.634532, 'Regional V Bandung') //Reg V Bandung
+        ->informationWindow(-7.014934, 110.423983, 'Regional VI Semarang') //Reg VI Semarang
+        ->informationWindow(-7.235923, 112.734074, 'Regional VII Surabaya') //Reg VII Surabaya
+        ->informationWindow(-8.666576, 115.231517, 'Regional VIII Denpasar') //Reg VIII Denpasar
+        ->informationWindow(-3.440981, 114.831636, 'Regional IX Banjarbaru') //Reg IX Banjarbaru
+        ->informationWindow(-5.164130, 119.434747, 'Regional X Makassar') //Reg X Makassar
+        ->informationWindow(-2.522577, 140.713324, 'Regional XI Jayapura'); //Reg XI Jayapura
 
         return view('pages.pusat');
     }
